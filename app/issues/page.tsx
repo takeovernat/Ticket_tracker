c7c4d618b84db9a3c4486ac90214d764ae5988c3
takeovernat <natnaelteshome9@gmail.com>
2024-01-29 06:13:05 -0600

    redix setup and button

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button>New Ticket</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
