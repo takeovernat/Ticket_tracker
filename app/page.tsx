72c0d2913a2289937cacde5e4d86e5b7d5613a97
takeovernat <natnaelteshome9@gmail.com>
2024-01-31 17:29:37 -0600

    dashboard layout change

--- app/page.tsx
+++ app/page.tsx
@@ -23,17 +23,17 @@ export default function Home() {
   }, []);
   console.log(data);
   return (
-    <div>
-      <div className="flex justify-start px-1">
+    <div className="">
+      <div className="flex justify-center px-7">
         <Button>
           <SiProtodotio />
           All Tickets
         </Button>
       </div>
       <Separator my="3" size="4" />
-      <div className="flex">
+      <div className="block">
         {Object.keys(data).length > 0 && (
-          <div className="py-5">
+          <div className="py-5 px-5 block ">
             {data.map(
               (issue: {
                 id: Key | number;
@@ -41,7 +41,7 @@ export default function Home() {
                 description: string;
                 status: string;
               }) => (
-                <div key={issue.id} className="my-2">
+                <div key={issue.id} className="my-2 px-2">
                   <TicketCard
                     title={issue.title}
                     description={issue.description}
